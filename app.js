ece963523d1c963079086c7ac5484c1268952afd
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-15 02:52:39 +0530

    todolist

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+var tasks = [];
+
 app.set("view engine", "ejs");
 
 app.get("/", function (req,res){
@@ -10,12 +15,31 @@ app.get("/", function (req,res){
     var today = new Date();
     var dayName = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     var daynum = today.getDay();
+
+
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    }
+
+    var day = today.toLocaleDateString('en-US', options);
+
+
     console.log(dayName[daynum])
 
     res.render("list", {
-        nameOfDay: dayName[daynum]
-    });  
+        nameOfDay: day,
+        listOfTasks : tasks,
+    });
 });
+
+app.post("/",function (req,res){
+    var newTask = req.body.newItem;
+    tasks.push(newTask);
+    res.redirect("/");
+});
+
 
 app.listen(3000, function (){
     console.log('This server is active on port 3000.')
